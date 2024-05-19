95863d8bad2a74db41565eff7e96854ad695926f
webmmr <mdmizanur3950@gmail.com>
2024-05-20 01:22:22 +0600

    feat: addeds statis pages and layouts with tailwind css

---
+++ app/_components/Logo.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-4 z-10">
+      <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <span className="text-xl font-semibold text-primary-100">
+        The Wild Oasis
+      </span>
+    </a>
+  );
+}
